4d78d8857aedebb49bef1b1e443a8893f4a0393e
Reuven <retzion@gmail.com>
2020-12-14 10:26:57 -0700

    New repo name

---
+++ README.md
@@ -0,0 +1,7 @@
+# nodejs-watermark
+
+1. Install GraphicMagick on your machine (or a buildpack on your server)
+2. Clone git repo: `git@github.com:retzion/nodejs-watermark.git`
+3. `npm i`
+4. `npm run start:local`
+5. Try it: `http://localhost:3001/https%3A%2F%2Fpicsum.photos%2F1200%2F777`
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-if (process.env.NODE_ENV !== "production") require("dotenv").config()
-
 const gm = require("gm")
 const request = require("request")
 const express = require("express")
@@ -12,22 +10,16 @@ function convertImage(inputStream) {
   return gm(inputStream)
     .resize(600, 336)
     .composite("watermark.png")
-    .geometry("+60+60")
+    .geometry("+30+30")
     .stream()
 }
 
-app.get("/r/:photoReference", function (req, res) {
-  const url = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=1200&photoreference=${req.params.photoReference}&key=${process.env["GOOGLE_PLACES_API_KEY"]}`
-  res.set("Content-Type", "image/png")
-  convertImage(request.get(url)).pipe(res)
-})
-
-app.get("/u/:url", function (req, res) {
+app.get("/:url", function (req, res) {
   const url = decodeURIComponent(req.params.url)
   res.set("Content-Type", "image/png")
   convertImage(request.get(url)).pipe(res)
 })
 
 app.listen(port, function () {
-  console.log(`ImageOptimizer listening on port ${port}!`)
+  console.log(`Node Watermark listening on port ${port}!`)
 })
